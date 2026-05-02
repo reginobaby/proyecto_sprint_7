878cfedf0e3e492850e22e899ac4e5e1616172eb
reginobaby <angelregcab22@gmail.com>
2026-05-01 18:43:14 -0600

    Proyecto terminado: app.py funcional con botones y gráficas

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # proyecto_sprint_7
+Este proyecto es una aplicación web interactiva creada con Streamlit para analizar un conjunto de datos de anuncios de vehículos. Permite a los usuarios generar histogramas y gráficos de dispersión de forma sencilla
--- app.py
+++ app.py
@@ -7,3 +7,25 @@ st.header('Análisis de anuncios de coches - reginobaby')
 
 # Proceso de lectura de datos CSV
 car_data = pd.read_csv("vehicles_us.csv")
+
+# Crear boton para el histograma
+hist_button = st.button("construir histograma")
+
+if hist_button:  # comando que se usa cuando hacen click al boton
+    st.write(
+        "Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
+
+# creación de histograma
+fig = px.histogram(car_data, x="odometer")
+
+# codigo que muestra el grafico plotly interactivo
+st.plotly_chart(fig, use_container_width=True)
+
+# Boton para grafico de disperción
+scatter_button = st.button("Construir gráfico de dispersión")
+
+# Creación de grafico de disperción
+fig_scatter = px.scatter(car_data, x="odometer", y="price")
+
+# mostrar el gráfico
+st.plotly_chart(fig_scatter, use_container_width=True)
